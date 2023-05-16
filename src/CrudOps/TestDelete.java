65509ca4e1f2db5c6acc39032deba6d9456bbf4d
Rahul Jha <rjrahuljha02@gmail.com>
2023-05-16 14:55:32 +0530

    Code Commit

--- src/CrudOps/TestDelete.java
+++ src/CrudOps/TestDelete.java
@@ -1,2 +1,31 @@
-package CrudOps;public class TestDelete {
+package CrudOps;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestDelete {
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            String url = "jdbc:mysql://localhost:3306/m10_rj";
+            String user = "root";
+            String pass = "rahul";
+
+            Connection con = DriverManager.getConnection(url, user, pass);
+            Statement stmt = con.createStatement();
+
+            String query = "DELETE FROM student WHERE id=131";
+            boolean execute = stmt.execute(query);
+
+            System.out.println(execute);
+
+            con.close();
+
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/CrudOps/TestInsert.java
+++ src/CrudOps/TestInsert.java
@@ -1,2 +1,32 @@
-package CrudOps;public class TestInsert {
+package CrudOps;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestInsert {
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            String url = "jdbc:mysql://localhost:3306/m10_rj";
+            String user = "root";
+            String pass = "rahul";
+
+            Connection con = DriverManager.getConnection(url, user, pass);
+            Statement stmt = con.createStatement();
+
+            String query = "INSERT INTO student VALUES" + "(131, 'Abhimanyu', 'abhi_man@gmail.com', 'MEAN')";
+            boolean execute = stmt.execute(query);
+
+            System.out.println(execute);
+
+
+
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
