506b065e1e46f6776c874a4191e7ebd920ad4d6e
Rahul Jha <rjrahuljha02@gmail.com>
2023-05-16 15:46:50 +0530

    Update Code Commit

---
+++ src/CrudOps/TestUpdate.java
@@ -0,0 +1,32 @@
+package CrudOps;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestUpdate {
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            String url = "jdbc:mysql://localhost:3306/m10_rj";
+            String user = "root";
+            String pass= "rahul";
+
+            Connection con = DriverManager.getConnection(url, user, pass);
+            Statement stmt = con.createStatement();
+
+            String query = "UPDATE student SET name='Abhinandan' WHERE id=131";
+
+            boolean execute = stmt.execute(query);
+            System.out.println(execute);
+
+            con.close();
+
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
