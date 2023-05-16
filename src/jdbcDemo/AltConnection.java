297b2dfda8d67e889a0f8e39bfbee546ac772107
Rahul Jha <rjrahuljha02@gmail.com>
2023-05-16 17:06:39 +0530

    Establishing the AltConnection

--- src/jdbcDemo/AltConnection.java
+++ src/jdbcDemo/AltConnection.java
@@ -5,6 +5,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class AltConnection {
+    /* In this file we are establishing the connection by using the overloaded method of getConnection(url),
+       which accepts only single arg that is URL. This url contains username and password. Although this is not a
+       recommended way due to security purposes, as it is less secure.
+    */
     public static void main(String[] args) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
